e6064a166af04abb82f62ecfcb0a54c6e486f586
woonieyoon <sungwon126@naver.com>
2019-01-28 17:55:41 +0900

    callable을 사용한 중복넘버제거

--- src/kr/sys4u/unique/collable/RandomNumberAdder.java
+++ src/kr/sys4u/unique/collable/RandomNumberAdder.java
@@ -1,32 +1,14 @@
 package kr.sys4u.unique.collable;
 
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Callable;
 
-public class RandomNumberAdder implements Callable<List<Integer>> {
-
-	private List<Integer> genreated;
-
-	public RandomNumberAdder(List<Integer> generated) {
-		this.genreated = generated;
-	}
+public class RandomNumberAdder implements Callable<Integer> {
 
 	@Override
-	public List<Integer> call() throws Exception {
-		try {
-			Thread.sleep(10L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		while (true) {
-			int randomNumber = new Random().nextInt(100);
-			if (!genreated.contains(randomNumber)) {
-				genreated.add(randomNumber);
-				return genreated;
-			}
-		}
+	public Integer call() throws Exception {
+		Thread.sleep(10);
+		return new Random().nextInt(100);
 	}
 
 }
--- src/kr/sys4u/unique/collable/UniqueNumberExecutor.java
+++ src/kr/sys4u/unique/collable/UniqueNumberExecutor.java
@@ -1,34 +1,55 @@
 package kr.sys4u.unique.collable;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class UniqueNumberExecutor {
 
 	public static void main(String[] args) {
-
-		List<Integer> list = new ArrayList<>();
-		ExecutorService executor = Executors.newFixedThreadPool(100);
+		Set<Integer> list = new TreeSet<>();
 
 		long startTime = System.nanoTime();
-		for (int i = 0; i < 100; i++) {
-			Future<List<Integer>> s = executor.submit(new RandomNumberAdder(list));
+
+		while (list.size() < 100) {
+			List<Future<Integer>> futures = new ArrayList<>();
+			ExecutorService executor = Executors.newFixedThreadPool(100);
+			futures.add(executor.submit(new RandomNumberAdder()));
+
+			for (int i = 0; i < 100; i++) {
+				futures.add(executor.submit(new RandomNumberAdder()));
+			}
+
+			executor.shutdown();
 			try {
-				list = s.get();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
+				executor.awaitTermination(5, TimeUnit.MINUTES);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+
+			for (Future<Integer> future : futures) {
+				if (list.size() >= 100) {
+					break;
+				}
+
+				try {
+					list.add(future.get());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+
+
 		System.out.println((System.nanoTime() - startTime) / 1000_000 + "ms");
-		Collections.sort(list);
 		list.stream().forEach(System.out::println);
-		
 	}
 }
